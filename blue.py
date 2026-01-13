c57202da73bca8505532a42d556831f84cdb2f7d
zhelukq <zhenya.orsk@gmail.com>
2026-01-13 10:29:17 +0500

    Update blue.py

--- blue.py
+++ blue.py
@@ -10,7 +10,7 @@ import os
 import urllib.request
 from datetime import datetime
 
-__VERSION__ = "1.0.3"
+__VERSION__ = "1.0.4"
 GITHUB_RAW_URL = "https://raw.githubusercontent.com/zhelukq/blue/main/blue.py"
 SCRIPT_PATH = os.path.abspath(__file__)
 
